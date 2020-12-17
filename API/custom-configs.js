443daf3bb6838c8d01aae9d533335c71f4b8d509
GnanaPradeepRS <gnana.gundluru@riversand.com>
2020-12-17 17:40:18 +0530

    APIs implemented

--- API/custom-configs.js
+++ API/custom-configs.js
@@ -3,13 +3,31 @@ const customConfigs = (app) => {
     app.post('/custom-configs/get' , async(req  , res) => {
         let tenantDetails = req.body.params.tenantDetails
         let configs=req.body.params.configs
+        console.clear()
         for (const x of configs) {
-            console.log(x)
+            let data = null
+            switch (x) {
+                case 'graphprocess':
+                    data = JSON.stringify({ "params": { "query": { "filters": { "typesCriterion": ["graphprocessmodel"] } }, "fields": { "attributes": ["_ALL"], "relationships": ["_ALL"] } } });
+                    configAPI.fetch(tenantDetails , data , '/api/entitymodelservice/get');
+                    break;
+
+                case 'profiles':
+                    data = JSON.stringify({ "params": { "query": { "contexts": [{}], "filters": { "typesCriterion": ["integrationProfile"] } }, "allContextual": true, "fields": { "properties": ["_ALL"], "attributes": ["_ALL"], "relationships": ["_ALL"] } } });
+                    configAPI.fetch(tenantDetails , data , '/api/configurationservice/get')
+                    break;
+
+                case 'uiconfigs':
+                    data = JSON.stringify({"params":{"query":{"contexts":[{}],"filters":{"typesCriterion":["uiconfig"],"allContextual":true}},"allContextual":true,"fields":{"properties":["_ALL"],"attributes":["_ALL"],"relationships":["_ALL"]}}});
+                    configAPI.fetch(tenantDetails , data , '/api/configurationservice/get');
+                    break;
+            
+                default:
+                    break;
+            }
         }
-let data = JSON.stringify({ "params": { "query": { "contexts": [{}], "filters": { "typesCriterion": ["integrationProfile"] } }, "allContextual": true, "fields": { "properties": ["_ALL"], "attributes": ["_ALL"], "relationships": ["_ALL"] } } });
- 
-        // configAPI.configAPI(tenantDetails,data)
-        // res.send(JSON.stringify(tenantDetails));
+
+        res.send("url here");
     })
 }
 
